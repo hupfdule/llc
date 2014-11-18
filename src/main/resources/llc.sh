371d0ea4a2c9263b17b06b3676ed84cb82567ae8
Marco Herrn <marco@mherrn.de>
2014-11-18 18:06:23 +0100

    Provide Shellscript

---
+++ src/main/resources/llc.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+get_first() {
+  echo $1
+}
+
+# We need to refence the tools.jar
+if [ -n "$JAVA_HOME" ]; then
+  # If JAVA_HOME is set, use its tools.jar
+  TOOLS_JAR=$JAVA_HOME/lib/tools.jar
+else
+  # If JAVA_HOME is not set, try to search for it from the java executable
+  WHICH_JAVA=$(which java)
+  if [ -n "$WHICH_JAVA" ]; then
+    TOOLS_JAR=$(dirname $WHICH_JAVA)/../lib/tools.jar
+  else 
+    # If that doesn't work, too, try to find it under /usr/lib/jvm/
+    TOOLS_JAR_FROM_USR_LIB_JVM=$(ls /usr/lib/jvm/*/lib/tools.jar)
+    if [ -n "$TOOLS_JAR_FROM_USR_LIB_JVM" ]; then
+      TOOLS_JAR=$(get_first "$TOOLS_JAR_FROM_USR_LIB_JVM")
+    else
+      # If even that didn't work, assume the tools.jar is already set in
+      # the $CLASSPATH
+      true
+    fi
+  fi
+fi
+
+# The application jar file
+JAR_FILE=llc.jar
+CLASSPATH=$CLASSPATH:$JAR_FILE
+# If we found a tools.jar, append it to the classpath
+if [ -n "$TOOLS_JAR" ]; then
+  CLASSPATH="$CLASSPATH":"$TOOLS_JAR"
+fi
+
+#now run the application
+java -cp $CLASSPATH eu.herrn.loglevelchanger.LogLevelChanger $@
