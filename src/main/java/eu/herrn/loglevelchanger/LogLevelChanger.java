64f4ae44b108cd0287d54efc61d689c58edb25ce
Marco Herrn <marco@mherrn.de>
2020-02-18 09:14:15 +0100

    Specify license as GPL 2 with classpath exception
    Since we copied a method from the JConsole source code we need to apply
    to its license which is GPL 2 with classpath exception.

--- src/main/java/eu/herrn/loglevelchanger/LogLevelChanger.java
+++ src/main/java/eu/herrn/loglevelchanger/LogLevelChanger.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2020, Marco Herrn.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  This particular file is
+ * subject to the "Classpath" exception as provided in the LICENSE file
+ * that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, see <http://www.gnu.org/licenses/>.
+ */
 package eu.herrn.loglevelchanger;
 
 import com.sun.tools.attach.AttachNotSupportedException;
--- src/main/java/eu/herrn/loglevelchanger/Options.java
+++ src/main/java/eu/herrn/loglevelchanger/Options.java
@@ -1,7 +1,21 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2020, Marco Herrn.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  This particular file is
+ * subject to the "Classpath" exception as provided in the LICENSE file
+ * that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, see <http://www.gnu.org/licenses/>.
  */
 
 package eu.herrn.loglevelchanger;
